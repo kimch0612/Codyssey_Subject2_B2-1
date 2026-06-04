c3065298db5b758ed319a160f2cf0d90b8ddb5fd
Changhwan Kim <kimch061279@gmail.com>
2026-06-04 13:36:21 +0900

    구조체 확정

--- budget_app/data_struct.py
+++ budget_app/data_struct.py
@@ -7,22 +7,22 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Transaction:
-    id: str = ""
-    type: str = ""
-    date: str = ""
-    amount: int = 0
-    category: str = ""
-    memo: str = ""
-    tags: list[str] = field(default_factory=list)
+    id: str = ""        # unique 값
+    type: str = ""      # 저장될 값을 income, expense로 제한
+    date: str = ""      # YYYY-MM-DD
+    amount: int = 0     # 금액
+    category: str = ""  # 카테고리 이름 -> Category.name과 연결
+    memo: str = ""      # 메모는 선택사항
+    tags: list[str] = field(default_factory=list) # 최종적으론 이런 구조가 되는 게 어떨까: tags = ["식비", "영화", "월정액"]
 
 
 @dataclass
 class Category:
     name: str = ""
+    #category_description: str = "" # 쓸 일이 있으려남
 
 
 @dataclass
 class Budget:
-    month: str = ""
+    month: str = ""     # YYYY-MM
     amount: int = 0
-
